317ef79d6d21e4ff792f1fa592d9e28033a0e366
Helg4s <oleg2703lebid@gmail.com>
2024-10-05 16:31:53 +0300

    Reapply "mok"
    This reverts commit 998aa12cd7635520a5c90a49d9c63bb39a07d702.

--- task/calculator.js
+++ task/calculator.js
@@ -2,6 +2,7 @@ const calculator = {
     add: (a, b) => a + b,
     subtract: (a, b) => a - b,
     multiply: (a, b) => a * b,
+    square: (a) => sqrt,
     divide: (a, b) => {
         if (b === 0) {
             throw new Error("Cannot divide by zero");
